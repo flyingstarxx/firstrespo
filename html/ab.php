56c9cd2ea8e5acc056943f2c39ea52edc225e36b
Wang Tianxing <tx951121@mail.ustc.edu.cn>
2017-08-11 08:13:48 +0800

    firstxiugai

--- html/ab.php
+++ html/ab.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
 $username = array(1,2,3,4)
-
+//ceshi
 $json_arr = array("ydata"=>$username);
 $json_obj = json_encode($json_arr);
 echo $json_obj;
